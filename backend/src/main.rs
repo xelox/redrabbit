dae0a29f92969e4a58ad1080528e605de0479439
xelox <alex@gorodoro.dev>
2024-07-17 19:21:42 +0300

    added api router + refactored serve_app handle

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,14 @@
-use tokio_util::io::ReaderStream;
-use axum::{body::Body, http::{header, HeaderMap, StatusCode}, response::IntoResponse, routing::get, Router};
+use axum::{ http::{header, HeaderMap}, response::IntoResponse, routing::get, Router};
 use tower_http::{cors::{self, CorsLayer}, services::ServeDir};
 pub mod database;
+pub mod api;
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/app", get(serve_app))
+        .nest("/api", api::api_router())
         .layer(CorsLayer::new().allow_origin(cors::Any))
         .nest_service("/assets", ServeDir::new("../frontend/dist/assets"))
         .nest_service("/", ServeDir::new("../../frontend/dist"));
@@ -17,16 +18,11 @@ async fn main() {
 }
 
 pub async fn serve_app() -> impl IntoResponse {
-    let file = match tokio::fs::File::open("../frontend/dist/index.html").await {
-        Ok(file) => file,
-        Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {}", err))),
-    };
-    let stream = ReaderStream::new(file);
-    let body = Body::from_stream(stream);
+    let html = include_str!("../../frontend/dist/index.html");
 
     let mut headers = HeaderMap::new();
     headers.insert(header::CONTENT_TYPE, "text/html; charset=utf-8".parse().unwrap());
     headers.insert(header::CONTENT_DISPOSITION, "inline; filename=\"index.html\"".parse().unwrap());
 
-    Ok((headers, body))
+    <(HeaderMap, &str) as IntoResponse>::into_response((headers, html))
 }
